8997c97c379bf13fed32dc3496e5fafd2308c7c0
Netcie <netcie@vslcatena.nl>
2017-07-23 22:39:31 +0200

    Buttons,columns

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
-<?php
-echo "Leeg";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<link href="./css/index.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+	</script>
+	<title></title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1 view">
+				<div class="btn-group-vertical">
+					<button class="btn btn-primary btn-sq" type="button">view_01</button> 
+					<button class="btn btn-primary btn-sq" type="button">view_02</button> 
+					<button class="btn btn-primary btn-sq" type="button">view_03</button> 
+					<button class="btn btn-primary btn-sq" type="button">view_04</button>
+				</div>
+			</div>
+			<div class="col-md-4 calc">
+				Calculator
+			</div>
+			<div class="col-md-7">
+				<div class="row category">
+					<div class="col-md-12">
+						<div class="btn-group btn-group-justified">
+							<a class="btn btn-primary btn-sq">Cat_01</a> 
+							<a class="btn btn-primary btn-sq">Cat_02</a> 
+							<a class="btn btn-primary btn-sq">Cat_03</a> 
+							<a class="btn btn-primary btn-sq">Cat_04</a> 
+							<a class="btn btn-primary btn-sq">Cat_05</a>
+						</div>
+					</div>
+				</div>
+				<div class="row products">
+					<div class="col-md-12">
+						<div class="btn-group-vertical btn-sq-prod">
+							<button class="btn btn-primary prod" type="button">prod_01</button> 
+							<button class="btn btn-primary prod_decr" type="button">prod_01_decr</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
