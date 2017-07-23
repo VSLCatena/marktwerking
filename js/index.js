0c742540190b883719a9e7eeee50135c4acb5d98
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-07-24 00:23:53 +0200

    Added: Working timer at top of the page
    It now has an update function that updates the time on the top every
    second. It also contains the code to update the graph every X seconds
    which can be changed in the settings at the top of the page.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+// Settings
+var updateTime = 120; // The update time in seconds
+var timeOffset = 5; // the amount of seconds we update LATER to avoid update issues=
+// End settings
+
+
+
+
 var statistics = new Statistics(document.getElementById('statistics'));
 
 function load(){
@@ -6,4 +14,32 @@ function load(){
     });
 }
 
-load();
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+var timeToUpdate = 0;
+function tick(){
+
+    if(new Date().getTime() > timeToUpdate){
+        load();
+
+        timeToUpdate = (parseInt(new Date().getTime() / (updateTime * 1000)) + 1) * updateTime * 1000;
+
+        // We update a couple of seconds later for possible update issues and such
+        timeToUpdate += timeOffset * 1000;
+    }
+
+    // Update timer on top
+    var secondsLeft = ((timeToUpdate - new Date().getTime()) / 1000) % updateTime;
+    var minutes = parseInt(secondsLeft / 60);
+    var seconds = Math.floor(secondsLeft % 60);
+    $("#timer").html(pad(minutes, 2) + " min. " + pad(seconds, 2) + " sec.");
+
+    // Update a second later
+    setTimeout(tick, 1000);
+}
+
+tick();
