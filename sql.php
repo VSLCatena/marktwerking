0d6d1fcee0e5b68f89650a01a48d47c173bfb859
Kipjr <martijn9873@hotmail.com>
2017-09-01 11:55:29 +0200

    Angular to PHP to SQL test
    Settings are updated; Category names are updated; items are updated; categorydrinks are updated; TODO creation of items,category rows when needed

--- sql.php
+++ sql.php
@@ -8,16 +8,37 @@ $data = array();
 
 $new_data = json_decode(file_get_contents('php://input'), true);
 print_r($new_data);
-if (!empty($new_data)){
+if (!empty($new_data)) {
 
-    foreach ($new_data as $key => $value) {
-        //$sql = "UPDATE settings SET lastname='Doe' WHERE id=2";
-        //$stmt = $conn->prepare($sql[$key]);
-        //$stmt->execute();
-        //print_r($value);
+    foreach ($new_data['settings'] as $key => $value) {
+        $value =
+        $sql = "UPDATE `settings` SET `value` = '$value' WHERE `settings`.`setting` = '$key';";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+    foreach ($new_data['category'] as $key => $value) {
+        $sql = "UPDATE `categories` SET `name` = '$value[name]' WHERE `categories`.`id` = '$value[id]';";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    }
+    foreach ($new_data['items'] as $key => $value) {
+        $sql = "UPDATE `drinks` SET `name` = '$value[name]', `start_price` = '$value[start_price]', `minimum_price` = '$value[minimum_price]', `active` = '$value[active]' WHERE `drinks`.`id` = '$value[id]';";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
     }
 
+    //first clear table of drinks&category
+    $sql = "TRUNCATE TABLE `drink_category`;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    foreach ($new_data['items'] as $key => $value) {
+        foreach ($value['categories'] as $key2 => $value2) {
+            $sql = "INSERT INTO `drink_category` (`drink_id`, `category_id`) VALUES ('$value[id]', '$value2');";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+        }
 
+    }
 }
 
 
