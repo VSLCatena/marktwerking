9afc31f7818ad0e854c5cc32e85478aea310d9ca
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-09-03 01:25:17 +0200

    Added: The old algorithm for calculating prices.
    We are now using the old algorithm for calculating the prices.
    
    This was a nightmare to convert.

--- prices.php
+++ prices.php
@@ -1,118 +1,127 @@
 <?php
 require_once('core.php');
+$timeframe = 10 * 60;
 
-class Product implements JsonSerializable {
-    private $id;
-    private $name;
-    private $minimumPrice;
-    private $startPrice;
-    private $prices;
-
-    function __construct($id, $name, $startPrice, $minimumPrice){
-        $this->id = $id;
-        $this->name = $name;
-        $this->startPrice = $startPrice;
-        $this->minimumPrice = $minimumPrice;
-    }
-
-    /**
-     * This is the calculating function.
-     * This should contain the algorithm used to determine the prices.
-     *
-     * @param array $roundAmounts the amount that have been sold on this product per round.
-     * @param array $totalsPerRound the amount that have been sold in the complete round (So all products)
-     * @param int $amountOfItems the amount of items that are in the game
-     *
-     * @return array prices per round
-     */
-    function calculate($roundAmounts, $totalsPerRound, $amountOfItems){
-        $prices = array();
-        $prices[0] = $this->startPrice;
-
-        for($i = 0; $i < count($roundAmounts); $i++){
-            $percentage = $roundAmounts[$i] / ($totalsPerRound[$i] / $amountOfItems);
-
-            $prices[$i+1] = max(
-                $prices[$i] * max(
-                    log($percentage, 1.5),
-                    0.6
-                ),
-                $this->minimumPrice
-            );
-
-            // Round it to the nearest 5 cents
-            $prices[$i+1] = round($prices[$i+1]*20)/20;
-        }
-
-        $this->prices = $prices;
-    }
-
-    public function getPrices(){
-        return $this->prices;
-    }
-
-    public function getLatestPrice(){
-        return $this->prices[count($this->prices)-1];
-    }
-
-    public function jsonSerialize(){
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'prices' => $this->prices,
-        ];
-    }
+function roundTo5s($num){
+    return round($num*20)/20;
 }
 
-// Create an array with all the products
-$ptsd = $pdo->prepare('SELECT * FROM drinks');
-$ptsd->execute();
+// Grab all the drinks and link them with the correct ID in the array
+$query = $pdo->query('SELECT * FROM drinks');
+$drinks = array();
+$drinksFinal = array();
+$prices = array();
+foreach($query->fetchAll(PDO::FETCH_ASSOC) as $drink){
+    $drinks[$drink['id']] = $drink;
+    $drinks[$drink['id']]['price'] = $drink['start_price'];
+    $drinks[$drink['id']]['new_price'] = null;
 
-$products = array();
-foreach($ptsd->fetchAll() as $row){
-    $products[$row['id']] = new Product($row['id'], $row['name'], $row['start_price'], $row['minimum_price']);
-}
-
-// Get a list of all average buying amounts
-$ptsd = $pdo->prepare('SELECT SUM(amount) as amount, FLOOR(UNIX_TIMESTAMP(date) / (10 * 60)) AS timeframe FROM orders GROUP BY timeframe ORDER BY timeframe ASC');
-$ptsd->execute();
-$roundAmounts = array();
-
-foreach($ptsd->fetchAll() as $row){
-    $roundAmounts[$row['timeframe']] = $row['amount'];
+    // Set up final stuff
+    $drinksFinal[$drink['id']]['id'] = $drink['id'];
+    $drinksFinal[$drink['id']]['name'] = $drink['name'];
+    $drinksFinal[$drink['id']]['prices'] = array();
+    $prices[0][$drink['id']] = $drink['start_price'];
 }
 
 
-// Get a list of amount per product per timeframe
-$ptsd = $pdo->prepare('SELECT * FROM order_history');
-$ptsd->execute();
-$amounts = array();
+// Next grab the list of order history
+$ptsd = $pdo->prepare('SELECT drink_id AS drink_id, sum(amount) AS amount, floor((unix_timestamp(date)) / ?) AS timeframe
+FROM orders
+WHERE date < ?
+GROUP BY orders.drink_id, floor((unix_timestamp(date)) / ?)
+ORDER BY floor((unix_timestamp(date)) / ?)');
+// We round to the closest timeframe, this is so both the bar and the projector will have accurate results.
+// We just need to make sure they're both loaded AFTER this timeframe happened.
+$ptsd->execute(array($timeframe, date("Y-m-d H:i:s", round(time() / $timeframe) * $timeframe), $timeframe, $timeframe));
+
+$orderHistory = array();
+$totals = array();
 foreach($ptsd->fetchAll(PDO::FETCH_ASSOC) as $row){
-    $amounts[$row['drink_id']][$row['timeframe']] = $row['amount'];
+    // If the timeframe totals doesn't exist yet, create it
+    if(!isset($totals[$row['timeframe']]))
+        $totals[$row['timeframe']] = 0;
+
+    // Add it to the timeframe totals
+    $totals[$row['timeframe']] += intval($row['amount']);
+    // And add the amount to the order history
+    $orderHistory[$row['timeframe']][$row['drink_id']] = intval($row['amount']);
 }
 
-
-// We want to fill in the blanks, for when a product hasn't been bought all round
-foreach(array_keys($amounts) as $id){
-    // So first we fill in the blanks
-    foreach(array_keys($roundAmounts) as $timeframe){
-        if(!array_key_exists($timeframe, $amounts[$id])){
-            $amounts[$id][$timeframe] = 0;
+// Now we loop over all drinks and set them to 0 if nothing has been purchased that round
+foreach($orderHistory as &$orderH){
+    foreach($drinks as $drink){
+        if(!array_key_exists($drink['id'], $orderH)){
+            $orderH[$drink['id']] = 0;
         }
     }
-    ksort($amounts[$id]);
-
-    // Then we want to normalize the indexes from timeframes to normal numbered indexes.
-    $amounts[$id] = array_values($amounts[$id]);
 }
 
-// At last, we'll normalize the indexes of the averages
-$roundAmounts = array_values($roundAmounts);
+// We loop over every order history and create the price with that.
+foreach($orderHistory as $timeframe => $history){
+    $total = $totals[$timeframe];
 
-// Now send all the data to the products for calculation
-foreach($products as $id => $product){
-    $product->calculate($amounts[$id], $roundAmounts, count($products));
+    // First we calculate the percentages per drink. This will be overwritten for every foreach
+    foreach($drinks as $id => &$drink){
+        $drink['percentage'] = round($history[$id]/$total * 100);
+    }
+
+    // Then we calculate how much the price descended.
+    // We use this to calculate the ascension prices.
+    $totalDescend = 0;
+    foreach($drinks as $id => &$drink){
+        if($drink['percentage'] < 15){
+
+            // Magic stuff
+            $diff = roundTo5s(round((10-$drink['percentage'])/10));
+            // Make sure it doesn't exceed the minimum price
+            $drink['new_price'] = max($drink['price'] - $diff, $drink['minimum_price']);
+            // Add the price to the totalDescend variable
+            $totalDescend += $drink['price'] - $drink['new_price'];
+        }
+    }
+
+    // For now we do this in a separate foreach as we want to keep the code as much the same as the old one
+    // For more easy debugging :3
+    $totalAscendPercentages = 0;
+    foreach($drinks as $drink){
+        if($drink['percentage'] > 15){
+            $totalAscendPercentages += $drink['percentage'];
+        }
+    }
+
+    foreach($drinks as $id => &$drink){
+        if($drink['percentage'] > 15){
+            $drink['ascendPercentage'] = round($drink['percentage'] / $totalAscendPercentages * 100);
+        }
+    }
+
+    $totalAscend = 0;
+    foreach($drinks as $id => &$drink){
+        if($drink['percentage'] > 15){
+            $diff = roundTo5s($drink['ascendPercentage'] * $totalDescend / 100);
+            $drink['new_price'] = $drink['price'] + $diff;
+            $totalAscend += $drink['new_price'] - $drink['price'];
+        }
+    }
+
+
+    // Now update every price to the new_price for the next loop
+    foreach($drinks as $id => &$drink){
+        // If we don't have a new price, we don't have to update anything
+        if($drink['new_price'] != null)
+            $drink['price'] = $drink['new_price'];
+
+        // Also set the new price to null for the next iteration
+        $drink['new_price'] = null;
+        // Also add it to the prices table
+        $prices[$timeframe][$id] = $drink['price'];
+    }
 }
 
-// Now we just want an array containing all the values without their keys. And use pretty printing because it's pretty <3
-echo json_encode(array_values($products), JSON_PRETTY_PRINT);
+foreach($prices as $timeframe => $priceList){
+    foreach($priceList as $key => $price){
+        $drinksFinal[$key]['prices'][] = doubleval($price);
+    }
+}
+
+echo json_encode(array_values($drinksFinal), JSON_PRETTY_PRINT);
